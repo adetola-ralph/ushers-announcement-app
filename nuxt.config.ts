ce8f3781c911bae8d58624e8da49a976b4281def
adetola-ralph <oreofe.olutola@gmail.com>
2024-10-25 17:08:31 +0100

    setup firebase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  nitro: {
+    firebase: {
+      gen: 2,
+    },
+    preset: "firebase",
+  },
+  modules: ["nuxt-vuefire", "@nuxtjs/tailwindcss"],
+  vuefire: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    },
+  },
+});
